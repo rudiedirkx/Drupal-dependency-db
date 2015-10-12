7daad098550c4e5b0a27a3e9fc6f392afef6a5aa
Rudie Dirkx <github@hotblocks.nl>
2015-10-13 00:40:11 +0200

    +creators *WIP* need more `modules` info *WIP*

---
+++ creators.php
@@ -0,0 +1,8 @@
+<?php
+
+require 'inc.config.php';
+
+header('Content-type: text/plain;charset=utf-8');
+
+// @todo Group by project_uid for `amount`
+// @todo Multiply by `num_downloads` for ~ `reach`
