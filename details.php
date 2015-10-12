b9ea627be54af33a7ebe73b825608ea15ad1f449
Rudie Dirkx <github@hotblocks.nl>
2015-10-13 00:46:40 +0200

    detail list sorting+

--- details.php
+++ details.php
@@ -9,25 +9,25 @@ if ( !$module ) {
 	exit("Need `module` in arg 1.\n");
 }
 
-$projects = $db->select_fields('modules', 'project_name', array('module_name' => $module));
+$projects = $db->select_fields('modules', 'project_name', 'module_name = ? ORDER BY project_name ASC', array($module));
 echo "Module `$module` exists in these projects:\n\n";
 ul($projects);
 
 echo "\n\n";
 
-$modules = $db->select_fields('modules', 'module_name', array('project_name' => $projects));
+$modules = $db->select_fields('modules', 'module_name', 'project_name IN (?) ORDER BY module_name ASC', array($projects));
 echo "Above " . count($projects) . " projects contain these " . count($modules) . " modules:\n\n";
 ul($modules);
 
 echo "\n\n";
 
-$dependencies = array_unique($db->select_fields('dependencies', 'dependency_module_name', array('module_name' => $module)));
+$dependencies = array_unique($db->select_fields('dependencies', 'dependency_module_name', 'module_name = ? ORDER BY dependency_module_name ASC', array($module)));
 echo "It has these " . count($dependencies) . " dependencies:\n\n";
 ul($dependencies);
 
 echo "\n\n";
 
-$dependees = array_unique($db->select_fields('dependencies', 'module_name', array('dependency_module_name' => $module)));
+$dependees = array_unique($db->select_fields('dependencies', 'module_name', 'dependency_module_name = ? ORDER BY module_name ASC', array($module)));
 echo "It is depended on by these " . count($dependees) . " modules:\n\n";
 ul($dependees);
 
